0a7a4f4220362b5eedeb0111b55c5f0e3511b3ed
hanita <hhaller@wisc.edu>
2025-02-22 17:37:50 -0500

    outline

---
+++ model/feed_forward_network.py
@@ -0,0 +1,8 @@
+from model.network import Network
+
+class FeedForwardNetwork(Network):
+    def __init__(self, config, number_of_neurons):
+        super().__init__(config, number_of_neurons)
+        # TODO
+
+    
--- model/network.py
+++ model/network.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
+import torch
 
 class Network(ABC):
     # TODO
-    def __init__(self, config):
+    def __init__(self, config, weights, number_of_neurons):
         self.config = config
+        self.state = torch.zeros(number_of_neurons)
