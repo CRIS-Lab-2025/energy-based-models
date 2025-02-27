352e277516795b08d7d6e09f34dbd35cd536d50c
hanita <hhaller@wisc.edu>
2025-02-27 11:52:03 -0500

    remove Layer object dependency from activation functions

--- core/activation.py
+++ core/activation.py
@@ -1,65 +1,57 @@
 import torch
-from _trash.layer import Layer, DropOutLayer
+from model._network import Network
 
-def get_activation(layer: Layer):
-    """Returns the given layer's activation function applied to it's state"""
-    if layer.activation == 'none':
+def get_activation(network: Network):
+    """Returns the given network's activation function applied to it's state"""
+    return get_activation(network.activation, network.state)
+    
+def get_activation(activation, neurons: torch.Tensor):
+    """Returns the given activation function applied to the given neuron values.
+
+    Args:
+        activation (string): the activation function to use. 'none', 'linear', 'sigmoid', 'hard_sigmoid', 'softmax', and 'dsilu' are all valid
+        neurons (torch.Tensor): the state values of the desired neurons.
+    """
+    if activation == 'none':
         pass
-    elif layer.activation == 'linear':
-        return linear(layer)
-    elif layer.activation == 'sigmoid':
-        return sigmoid(layer)
-    elif layer.activation == 'hard_sigmoid':
-        return hard_sigmoid(layer)
-    elif layer.activation == "softmax":
-        return softmax(layer)
-    elif layer.activation == 'dsilu':
-        return dSiLU(layer)
-    elif layer.activation == 'dropout':
-        return dropout(layer)
-    elif layer.activation == 'nonlinear_restitive':
-        return nonlinear_restitive(layer)
+    elif activation == 'linear':
+        return linear(neurons)
+    elif activation == 'sigmoid':
+        return sigmoid(neurons)
+    elif activation == 'hard_sigmoid':
+        return hard_sigmoid(neurons)
+    elif activation == "softmax":
+        return softmax(neurons)
+    elif activation == 'dsilu':
+        return dSiLU(neurons)
     else: 
-        raise ValueError('Unknown activation type: {}'.format(layer.activation_type))
+        raise ValueError('Unknown activation type: {}'.format(activation))
 
 
 
-def linear(layer: Layer):
+
+def linear(neurons: torch.Tensor):
     """Returns the value of the layer's state"""
-    return layer._state
+    return neurons
 
-def sigmoid(layer: Layer):
+def sigmoid(neurons: torch.Tensor):
     """Returns the logistic function applied to the layer's state"""
-    return torch.sigmoid(4 * layer._state - 2)
+    return torch.sigmoid(4 * neurons - 2)
 
-def hard_sigmoid(layer: Layer):
+def hard_sigmoid(neurons: torch.Tensor):
     """Returns the value of the layer's state, clamped between 0 and 1"""
-    return layer._state.clamp(min=0., max=1.)
+    return neurons.clamp(min=0., max=1.)
 
-def softmax(layer: Layer):
+def softmax(neurons: torch.Tensor):
     """Returns the softmax function applied to the layer's state"""
     # FIXME: computing the cross-entropy loss requires computing the log first.
     # Directly computing the log_softmax would be faster and would have better numerical properties.
-    return torch.nn.functional.softmax(layer._state)
+    return torch.nn.functional.softmax(neurons)
 
 # dSiLULayer includes auxiliary
-def auxiliary(state):
-    torch.sigmoid(3 * state) * state
+def auxiliary(neurons):
+    torch.sigmoid(3 * neurons) * neurons
 
-def dSiLU(layer: Layer):
+def dSiLU(neurons: torch.Tensor):
     """Returns the sigmoid-weighted linear function applied to the layer's state"""
-    return auxiliary(layer._state) - auxiliary(layer._state - 1.)
-
-# dropout includes draw_mask
-def dropout(layer: DropOutLayer):
-    """Activate the dropout layer"""
-    return 1./(1.-layer._sparsity) * layer._mask * layer._layer.activate()
-
-
-def nonlinear_restitive(layer: Layer):
-    """Returns the value of the layer's state, clamped between 0 and +infinity for excitatory units, and clamped between -infinity and 0 for inhibitory units"""
-        
-    dimension = layer._shape[0] // 2  # number of excitatory units = number of inhibitory units = number of units / 2
-    excitatory = layer._state[:,:dimension].clamp(min=0., max=None)  # the first half of the units are excitatory units
-    inhibitory = layer._state[:,dimension:].clamp(min=None, max=0.)  # the second half of the units are inhibitory units
-    return torch.cat((excitatory, inhibitory), 1)  # we concatenate excitatory and inhibitory units
+    return auxiliary(neurons) - auxiliary(neurons - 1.)
